8c6d8d93ab653de9697c09efadfa19f2877f1482
Nathan Hietala <Nrhietala@gmail.com>
2021-07-04 22:21:44 -0500

    Adds several new tables, works locally

--- data/migrations/00-users.js
+++ data/migrations/00-users.js
@@ -1,9 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", table => {
-    table.increments("user_id");
+    table.increments("id");
     table.string("username", 128).notNullable().unique();
     table.string("password", 128).notNullable();
-    table.bigInteger("phone_number").notNullable();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("telephone").notNullable();
+    table.timestamps(true, true);
   });
 };
 
